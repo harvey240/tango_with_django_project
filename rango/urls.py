b1931cc7583e69ea2871c43a3f23045aff3a5c01
Harvey Russell <2461525r@student.gla.ac.uk>
2021-01-17 17:02:39 +0000

    Chapter 3, url stuff

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 16:35:47 2021
+
+@author: Harvey
+"""
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+        path('', views.index, name='index'),
+        ]
