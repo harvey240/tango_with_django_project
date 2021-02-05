98e62cb8436992f07258015885e75a18edf44ec7
Harvey Russell <2461525r@student.gla.ac.uk>
2021-02-05 11:29:10 +0000

    chapter 10, changed helper methods for session data

--- rango/views.py
+++ rango/views.py
@@ -5,10 +5,39 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+
+#HELPER FUNCTION
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+#HELPER FUNCTION, for handling cookies
+def visitor_cookie_handler(request, response):
+    #get number of site visits, use COOKIES.get() to obtain visits cookie.
+    #if cookie exists, return value as int, if not value 1 is used
+    visits = int(request.COOKIES.get('visits','1'))
+    
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    
+    #if more than a day since last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        #update last_visit_cookie now we have updated count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        #set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    
+    #update/set the visits cookie
+    request.session['visits'] = visits
 
 
 def index(request):
-    
     # Query the database for a list of ALL categories currently stored.
     # Order the categories by the number of likes in descending order.
     # Retrieve the top 5 only -- or all if less than 5.
@@ -22,15 +51,20 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    context_dict['visits'] = int(request.COOKIES.get('visits','1'))
     
+    #obtain response object early on so we can add cookie info.
+    response = render(request, 'rango/index.html', context=context_dict)
     
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    #call the helper function to handle cookies
+    visitor_cookie_handler(request, response)
+    
+    # Return response back to user, updating any cookies that need changed
+    return response
 
 
 def about(request):
+    
     context_dict = {'name': 'Harvey Russell'}
     
     return render(request, 'rango/about.html', context=context_dict)
@@ -172,8 +206,7 @@ def register(request):
                   context = {'user_form': user_form,
                              'profile_form': profile_form,
                              'registered': registered})
-            
-    
+
     
 def user_login(request):
     #if HTTP POST, try pull relevant info
